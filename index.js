07f5b3a9464154cdced099d357c9fdc62180819e
alcotrinov <alvarocotrino@gmail.com>
2025-05-22 19:44:52 -0500

    se modifico el archivo index.js y se creo la conexion a la base de datos

--- index.js
+++ index.js
@@ -1,7 +1,28 @@
 const express = require ("express");
 const app = express();
-app.get("/", (req, res) => { // <-- Abre con {
-  res.send(" servidor funcionando");
-  }); // <-- Cierra con }
- app.listen(10000);
+const mongoose = require ("mongoose");
+const cors = require("cors");
+const morgan = require("morgan");
+const bodyparcer = require ( "body-parser");
+const bodyParser = require("body-parser");
+app.use(bodyParser.json()); 
+
+const postrutas = require("./rutas/post);")
+app.use("./servicios",postrutas);
+
+
+mongoose.connect("mongodb+srv://alvarocotrino:zf8Vu0JQkqvbqdKj@cluster0.dxd8od2.mongodb.net/lunes?retryWrites=true&w=majority&appName=Cluster0")
+    .then(() => console.log("¡Conectado a MongoDB Atlas con éxito!"))
+    .catch(err => console.error("Error al conectar a MongoDB Atlas:", err));  
+  
+  
+
+
+  //app.get("/", (req, res) => {  
+  //res.send(" servidor funcionando");
+  //});
+ 
+ 
+  
+  app.listen(10000);
  
