bbac360816323014d51c13de9000deb606fe1f7b
alvaro <alvarocontrino@hotmail.com>
2025-05-26 11:28:53 -0500

    cambios necesarios porque se dejo de conectar a mongo atlas

--- index.js
+++ index.js
@@ -4,27 +4,22 @@ import cors from "cors";
 import morgan from "morgan";
 import postrutas from "./routes/post.js"; // importo las rutas
 const app = express();
-app.use(express.json());
-app.use(cors());
-app.use(morgan("dev"));
-mongoose.connect(
-  "mongodb+srv://alvarocotrino:lw1IzDZm9wh2QPbOj@cluster0.dxd8od2.mongodb.net/lunes?retryWrites=true&w=majority&appName=Cluster0",
+const mongoose =require("mongoose")
+const cors= require("cors");
+const morgan = require("morgan");
+
+
+const postrutas=require("./routes/post");
+app.use("servicios",postrutas);
   
-    {
+mongoose.connect("mongodb+srv://alvarocotrino:lw1IzDZm9wh2QPbOj@cluster0.dxd8od2.mongodb.net/lunes?retryWrites=true&w=majority&appName=Cluster0", {
     useNewUrlParser: true,
     useUnifiedTopology: true
-  }
-);
+});
 
 const connection = mongoose.connection; // creo conexion a la base de datos mongoDB
 connection.once("open", () => {
-  console.log("MongoDB conexion exitosa a la base de datos establecida");
-  app.use("/servicios", postrutas); // Usar las rutas solo después de la conexión exitosa
-  app.listen(10000); // puerto por donde escucha
-});
-connection.on("error", (error) => {
-  console.log("Error de conexion a la base de datos", error);
-});
+   console.log("MongoDB conexion exitosa a la base de datos establecida");
 
-
+});
 app.listen(10000);
