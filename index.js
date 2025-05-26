3b22a97f28870edbcfef52bc14d90ac199efb252
alvaro <alvarocontrino@hotmail.com>
2025-05-25 21:29:20 -0500

    cambios

--- index.js
+++ index.js
@@ -1,15 +1,12 @@
-const express = require("express");
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import morgan from "morgan";
+import postrutas from "./routes/post.js"; // importo las rutas
 const app = express();
-const mongoose = require("mongoose");
-const cors = require("cors");
-const morgan = require("morgan");
 app.use(express.json());
 app.use(cors());
 app.use(morgan("dev"));
-
-const postrutas = require("./routes/post"); // importo las rutas
-app.use("/servicios", postrutas);
-
 mongoose.connect(
   "mongodb+srv://alvarocotrino:zf8Vu0JQkqvbqdKj@cluster0.dxd8od2.mongodb.net/lunes?retryWrites=true&w=majority&appName=Cluster0",
   {
@@ -20,10 +17,13 @@ mongoose.connect(
 
 const connection = mongoose.connection; // creo conexion a la base de datos mongoDB
 connection.once("open", () => {
-  console.log("MongoDB conexion de la base de datos establecida");
+  console.log("MongoDB conexion exitosa a la base de datos establecida");
+  app.use("/servicios", postrutas); // Usar las rutas solo después de la conexión exitosa
+  app.listen(10000); // puerto por donde escucha
 });
 connection.on("error", (error) => {
   console.log("Error de conexion a la base de datos", error);
 });
 
-app.listen(10000); // puerto por donde escucha
+
+app.listen(10000);
